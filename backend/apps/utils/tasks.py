24864733d17c36dd92bb6119bfb51f0aca257b41
raeesiqbal <rayiszafar@gmail.com>
2023-11-01 19:12:53 +0500

    deleting media if ad is deleted

--- backend/apps/utils/tasks.py
+++ backend/apps/utils/tasks.py
@@ -1,8 +1,8 @@
 from celery import shared_task
-from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
+import boto3
+import environ
 
 
 @shared_task()
@@ -15,3 +15,22 @@ def send_email_to_user(title, html_message, plaintext_message, from_email, to_em
     )
     msg.attach_alternative(html_message, "text/html")
     msg.send()
+
+
+@shared_task()
+def delete_s3_object_by_urls(media):
+    s3_client = boto3.client("s3")
+    env = environ.Env()
+    bucket_name = env.str("S3_BUCKET_NAME")
+    objects_to_delete = []
+
+    for category, urls in media.items():
+        for url in urls:
+            object_key = url.replace(f"https://s3.amazonaws.com/{bucket_name}/", "")
+            objects_to_delete.append({"Key": object_key})
+
+    s3_client.delete_objects(
+        Bucket=bucket_name,
+        Delete={"Objects": objects_to_delete},
+    )
+    return True
