b15c831b43e7d1cdda01a5cbc15c09bc2b2cbd2a
raeesiqbal <rayiszafar@gmail.com>
2023-11-01 15:00:13 +0500

    delete urls

--- backend/apps/ads/views/ads_veiwset.py
+++ backend/apps/ads/views/ads_veiwset.py
@@ -141,6 +141,13 @@ class AdViewSet(BaseViewset):
 
     s3_service = S3Service()
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        
+        self.perform_destroy(instance)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
